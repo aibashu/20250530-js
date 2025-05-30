f2407b4c34d813ec9a5f119f5ecf795b75b74a54
aibashu <167419464+aibashu@users.noreply.github.com>
2025-05-30 18:39:38 +0900

    Add files via upload

---
+++ operator/js/script.js
@@ -0,0 +1,81 @@
+/*
+算術演算子
+*/
+
+// 加算
+let add = 10 + 5;
+
+console.log(add); //15
+
+// 文字列の連結
+let str = "Java" + "Script";
+console.log(str); //"javaScript"
+
+let result1 = 3 + 3;
+let result2 = "3" + "3";
+
+console.log(result1);
+console.log(result2);
+
+// 減算
+let sub = 10 - 5;
+console.log(sub); // 5
+
+// 乗算
+let mul = 10 * 5; // 10 ×　5
+console.log(mul); // 50
+
+// 除算
+let div = 10 / 5; // 10 ÷ 5
+console.log(div); // 2
+
+// インクリメント
+let incr = 10;
+incr++; // 現在の値から1を加える
+console.log(incr); // 11
+
+//　デクリメント
+let decr = 10;
+decr--; // 現在の値から1を引く
+console.log(decr);  // 9
+
+/*
+代入演算子
+*/
+// 加算して代入
+let add2 = 1;
+add2 += 2; // 1 + 2
+console.log(add2); // 3
+
+// 文字列を連結して代入
+let str2 = "Java";
+str2 += "Script"; // "Java" + "Script"
+console.log(str2); // "JavaScript"
+
+// 減算して代入
+let sub2 = 10;
+sub2 -= 5; // 10 - 5
+console.log(sub2); // 5
+
+// 乗算して代入
+let mul2 = 10;
+mul2 *= 5; // 10 x 5
+console.log(mul2); // 50
+
+// 除算して代入
+let div2 = 10;
+div2 /= 5; // 10 ÷ 5
+console.log(div2); // 2
+
+/*
+　比較演算子
+*/
+// 等しいかどうか
+let isEqual = (10 == "10"); // 値が等しい
+console.log(isEqual); // true 真
+
+// 等しくないかどうか
+let isNotEqual = (10 != 5); // 値が等しくない
+console.log(Greater); // true 
+
+
